1ff734863e4f793f1d32e6192bdcc7c6c9734475
hassene <hassene.elhabibi.prf@gmail.com>
2020-06-19 00:23:21 +0200

    Define the fucntion used to parse the current directory and the main function

--- dirWalker.py
+++ dirWalker.py
@@ -1,16 +1,33 @@
 import os
 
-for folderName, subfolders, filenames in os.walk(os.getcwd()):
-   print("The current folder is "+folderName)
-   if '.git' in subfolders:
-      subfolders.remove('.git')
-   for subfolder in subfolders:
-      #folder = str(subfolder)
-      #if not folder.startwith('.'):
-      print("SUBFOLDER of "+ folderName + ":" + subfolder)
+def parseDirectory(Current_path):
+   """
+   inputParm: Current_path => path that will parsed
+   Description: this function will print all folders, subfolders and filenames found in the current path
+                excluding the folders, subfolders and filename which start with a dot.
+   To conitune, please tape "q".
+   """
+   print(f"the directory which it will be parsed is {Current_path}")
+   for folderName, subfolders, filenames in os.walk(Current_path):
+      if str(os.path.basename(folderName)).startswith('.'):
+         continue
+      else:
+         print("the current folder is "+ folderName)
+         for subfolder in subfolders:
+            if str(subfolder).startswith('.'):
+               subfolders.remove(subfolder)
+            else:
+               print("SUBFOLDER of "+ folderName + ":" + subfolder)
+         for filename in filenames:
+            if str(filename).startswith('.'):
+               filenames.remove(filename)
+            else: 
+               print("File Inside" + folderName + ":" + filename)
+      print("===================================================================")
+def main():
+   current_path = os.getcwd()
+   print(help(parseDirectory))
+   parseDirectory(current_path)
 
-   for filename in filenames:
-       print("File Inside" + folderName + ":" + filename)
-
-   print("")
-
+if __name__ == "__main__":
+   main()
