738a6068df0c96542bf5e2cc9e764b388c3f49fa
hassene <hassene.elhabibi.prf@gmail.com>
2020-06-21 01:39:32 +0200

    adding writing to csv file feature

--- dirWalker.py
+++ dirWalker.py
@@ -1,4 +1,5 @@
 import os
+import csv
 """
 Stuff that will be tested:
 - Creating Regex Objects:
@@ -17,33 +18,46 @@ def parseDirectory(Current_path):
                 excluding the folders, subfolders and filename which start with a dot.
    To conitune, please tape "q".
    """
-   parser = open('parser.txt','a')
+   csvFileObj = open('/home/hassene/MyWorkspace/02_Dev/01_Scripts/01_Python/newFolder/dirParsers/parserCVS.csv', 'w', newline='')
+   csvWriter = csv.writer(csvFileObj)
+   #parser = open('parser.txt','a')
    print(f"the directory which it will be parsed is {Current_path}")
    for folderName, subfolders, filenames in os.walk(Current_path):
       if str(os.path.basename(folderName)).startswith('.'):
          continue
       else:
          #print("the current folder is "+ folderName)
-         parser.write("the current folderis " + folderName+ '\n')
+         #parser.write("the current folderis " + folderName+ '\n')
+         csvWriter.writerow(str(folderName))
          for subfolder in subfolders:
             if str(subfolder).startswith('.'):
                subfolders.remove(subfolder)
             else:
                #print("SUBFOLDER of "+ folderName + ":" + subfolder)
-               parser.write("SUBOLDER of "+ folderName+":"+ subfolder+ '\n')
+               #parser.write("SUBOLDER of "+ folderName+":"+ subfolder+ '\n')
+               csvWriter.writerow(str(subfolder))
          for filename in filenames:
             if str(filename).startswith('.'):
                filenames.remove(filename)
             else: 
                #print("File Inside" + folderName + ":" + filename)
-               parser.write("File Inside "+ folderName+":"+ filename+ '\n')
+               #parser.write("File Inside "+ folderName+":"+ filename+ '\n')
+               csvWriter.writerow(str(filename))
      # print("===================================================================")
-      parser.write("==========================================================================")
-   parser.close()
+     # parser.write("==========================================================================")
+   #parser.close()
+   csvFileObj.close()
+
+def csvWriter():
+   with open('/home/hassene/MyWorkspace/02_Dev/01_Scripts/01_Python/newFolder/dirParsers/parserCVS.csv', 'w', newline='') as csvfile:
+      spamwriter = csv.writer(csvfile)
+      spamwriter.writerow([1, 2, 3.141592, 4])
+
 def main():
    current_path = os.getcwd()
    print(help(parseDirectory))
    parseDirectory(current_path)
+ #  csvWriter()
 
 if __name__ == "__main__":
    main()
